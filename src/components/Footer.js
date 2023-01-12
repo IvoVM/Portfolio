1689a5b49fe7af86e7f18cc2723d152be458c38b
IvoVM <ivomastrangelo12@gmail.com>
2023-01-12 11:33:47 -0300

    Unused Imports removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 import navIcon4 from "../assets/img/nav-icon5.svg";
 
 export const Footer = () => {
@@ -11,7 +8,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
