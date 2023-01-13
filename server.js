4466825debf79b535eed0d449da22d4a411b3134
IvoVM <ivomastrangelo12@gmail.com>
2023-01-13 15:56:37 -0300

    Port changes

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const nodemailer = require("nodemailer");
 
 // server used to send send emails
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
